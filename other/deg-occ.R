fdc930f4b16f88c409de766ad7a3c4523f0f85e2
Ray Wright <raywright88@gmail.com>
2019-03-14 07:22:49 -0400

    Update script

--- other/deg-occ.R
+++ other/deg-occ.R
@@ -3,12 +3,12 @@
 library(tidyverse)
 library(acs)
 
-source("other/ignore/calc.R")
+source("other/calc.R")
 
 file_db  <- "~/data/acs/acsdb"
 file_occ <- "data-raw/cw-occupation.csv"
 years    <- 2014:2016
-vars     <- c("year", "perwt", "met2013", "sex", "age", "race", "hispan", "educd", "degfield", "occ2010", "incearn")
+vars     <- c("year", "perwt", "sex", "age", "race", "hispan", "educd", "degfield", "occ2010", "incearn")
 
 # funs --------------------------------------------------------------------
 
@@ -17,8 +17,7 @@ acs_clean2 <- function(data, file_occ) {
 
   data <- data %>%
     left_join(cw_occ, by = c("occ2010" = "occ_code")) %>%
-    mutate_at(c("occ_cat_name", "occ_name"), str_to_lower) %>%
-    select(year, perwt, met2013, sex, race, age, educd, degfield, occ_cat_name, occ_name, incearn)
+    mutate_at(c("occ_cat_name", "occ_name"), str_to_lower)
 
   data
 }
@@ -39,38 +38,22 @@ rec_coder <- function(x) {
   out
 }
 
-plot_age <- function(dists, by) {
-  by <- sym(by)
-
-  dists %>%
-    mutate(!!by := reorder(!!by, desc(earn_stat))) %>%
-    ggplot(aes(age, earn_stat, color = !!by)) +
-    geom_point(size = 2) +
-    geom_smooth(span = 1, se = FALSE, size = 0.5) +
-    scale_y_continuous(limits = c(0, NA), breaks = seq(0, 1e6, 2e4), labels = scales::comma) +
-    scale_color_brewer(type = "qual", palette = "Set1") +
-    theme_bw()
-}
-
 # run ---------------------------------------------------------------------
 
 dat <- acs_db_read(file_db, years = years, vars = vars)
 dat <- acs_clean(dat)
-dat <- acs_clean2(dat, file_occ = file_occ)
+# dat <- acs_clean2(dat, file_occ = file_occ)
+
+rwmisc::summary2(dat)
 
 dists <- dat %>%
-  filter(sex == "male", age %in% 25:54) %>%
-  mutate(occ_name = rec_coder(occ_name)) %>%
+  filter(race == "white", age %in% 30:49) %>%
   calc_dists(
-    by1 = c("age"),
-    by2 = c("occ_name"),
+    by1 = c("sex"),
+    by2 = c("degfield"),
     type = "median"
   )
 
 dists %>%
-  filter(occ_name == "coder") %>%
-  ggplot(aes(age, percent)) +
-  geom_point() +
-  geom_line() +
-  scale_y_continuous(limits = c(0, NA)) +
-  theme_bw()
+  filter(sex == "male") %>%
+  arrange(desc(earn_stat))
