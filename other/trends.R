66bc9b018d7be587d5a652cb30d5aa3d5b4058af
Ray Wright <raywright88@gmail.com>
2019-03-24 22:00:57 -0400

    Update script

--- other/trends.R
+++ other/trends.R
@@ -2,6 +2,7 @@
 
 library(tidyverse)
 library(acs)
+library(rwmisc)
 
 years <- 2010:2017
 
@@ -21,16 +22,26 @@ calc_by_year <- function(years) {
 
     data <- acs_db_read(file_db = file_db, years = year, vars = vars)
     data <- acs_clean(data)
-    data <- filter(data, age %in% 30:49)
+    data <- filter(data, age %in% 25:34)
+
+    data$educd[data$educd %in% c("less-hs", "hs-ged")] <- "hs-or-less"
+    data$educd[data$educd %in% c("bachelor", "advanced")] <- "bach-plus"
 
     out <- data %>%
       group_by(year, sex, educd, degfield) %>%
-      summarise(n = n(), pop = sum(perwt), earn_mean = weighted.mean(incearn, perwt)) %>%
+      summarise(
+        n = n(),
+        pop = sum(perwt),
+        earn_mean = round(weighted.mean(incearn, perwt), -3),
+        earn_p50 = round(Hmisc::wtd.quantile(incearn, perwt, probs = 0.5), -3)
+      ) %>%
       group_by(year, sex) %>%
-      mutate(d = pop / sum(pop)) %>%
+      mutate(percent = pop / sum(pop) * 100) %>%
       ungroup()
 
-    out[, c("year", "sex", "educd", "degfield", "n", "pop", "d", "earn_mean")]
+    out$earn_mean[out$n < 100] <- NA
+    out$earn_p50[out$n < 100] <- NA
+    out[, c("year", "sex", "educd", "degfield", "n", "pop", "percent", "earn_mean", "earn_p50")]
   })
 
   out <- bind_rows(out)
@@ -53,7 +64,7 @@ plot_trend <- function(data, y, color, facet = NULL, n_col = NULL) {
 
   p <- data %>%
     ggplot(aes(year, !!y, color = !!color)) +
-    geom_point(size = 1.5) +
+    geom_point(size = 2) +
     geom_line(size = 1) +
     ggrepel::geom_text_repel(
       mapping = aes(label = format(round(!!y * 100, 1), nsmall = 1)),
@@ -66,7 +77,7 @@ plot_trend <- function(data, y, color, facet = NULL, n_col = NULL) {
     scale_x_continuous(minor_breaks = NULL) +
     scale_y_continuous(limits = c(0, NA), minor_breaks = NULL) +
     scale_color_brewer(type = "qual", palette = "Set1") +
-    rwmisc::theme_rw()
+    theme_rw()
 
   if (!is.null(facet)) {
     p <- p + facet_wrap(facet, ncol = n_col)
@@ -81,17 +92,17 @@ system.time({
   res <- calc_by_year(years = years)
 })
 
-rwmisc::summary2(res)
+summary2(res)
 
-# res %>%
-#   filter(year == max(year), sex == "male") %>%
-#   mutate(percent = round(d * 100, 2)) %>%
-#   arrange(desc(pop))
-#
-# res %>%
-#   filter(year %in% c(2010, 2017), sex == "male") %>%
-#   mutate(percent = round(d * 100, 2)) %>%
-#   select(year, sex, educd, degfield, percent) %>%
-#   spread(year, percent) %>%
-#   mutate(diff = round(`2017` - `2010`, 2)) %>%
-#   arrange(desc(`2017`))
+res %>%
+  filter(year == max(year), sex == "male") %>%
+  mutate(degfield = str_sub(degfield, 1, 20)) %>%
+  arrange(desc(earn_p50))
+
+res %>%
+  filter(year %in% c(2010, 2017), sex == "male") %>%
+  select(year, sex, educd, degfield, percent) %>%
+  spread(year, percent) %>%
+  mutate(diff = `2017` - `2010`) %>%
+  mutate(degfield = str_sub(degfield, 1, 20)) %>%
+  arrange(desc(`2017`))
