2d5c0ee247f5b7f333916947e77256d315150068
Ray Wright <raywright@pa.gov>
2019-07-17 11:51:14 -0400

    Update script

--- other/cross-section.R
+++ other/cross-section.R
@@ -14,45 +14,32 @@ years <- 2017
 
 get_data <- function(file_db, years) {
   vars <- c(
-    "year", "perwt", "met2013", "sex", "age", "race", "hispan",
-    "educd", "degfield", "occ2010", "uhrswork", "incwage"
+    "year", "perwt", "met2013", "sex", "age", "race", "hispan", "educd",
+    "degfield", "occ2010", "wkswork2", "uhrswork", "incwage"
   )
   data <- acs::acs_db_read(file_db, years = years, vars = vars)
   data <- acs::acs_clean(data)
-
   cw_occ <- read_csv("data-raw/cw-occupation.csv", col_types = "icc")
   data <- left_join(data, cw_occ, by = c("occ2010" = "occ_code"))
   data <- mutate_at(data, c("occ_cat_name", "occ_name"), tolower)
   data
 }
 
-calc_stats <- function(data, by1, by2) {
-  by1_ <- syms(by1)
-  by2_ <- syms(by2)
-  n_years <- length(unique(data$year))
+calc_stats <- function(data, by) {
+  by_ <- syms(by)
   probs <- seq(0.1, 0.9, 0.01)
 
-  dists_tot <- data %>%
-    group_by(!!!by2_) %>%
-    summarise(pop = sum(perwt) / n_years) %>%
-    ungroup() %>%
-    mutate(perc_all = round(pop / sum(pop) * 100, 1)) %>%
-    select(-pop)
-
   out <- data %>%
-    group_by(!!!by1_, !!!by2_) %>%
+    group_by(!!!by_) %>%
     summarise(
       n = n(),
-      pop = sum(perwt) / !!n_years,
+      pop = sum(perwt),
       p = list(!!probs),
       q = list(rwmisc::wtd_quantile(incwage, perwt, probs = !!probs))
     ) %>%
-    group_by(!!!by1_) %>%
-    mutate(percent = round(pop / sum(pop) * 100, 1)) %>%
+    unnest() %>%
     ungroup()
 
-  out <- left_join(out, dists_tot, by = by2)
-  out <- unnest(out)
   out$q[out$n < 100] <- NA
   out
 }
@@ -69,7 +56,7 @@ plot_latest <- function(data, color = NULL) {
     geom_point(size = 1.5, alpha = 0.2) +
     geom_smooth(span = 0.5, se = FALSE, size = 1) +
     scale_x_continuous(limits = c(0, 1), breaks = seq(0, 1, 0.1), minor_breaks = NULL) +
-    scale_y_log10(breaks = seq(1e4, 2e5, 1e4), minor_breaks = NULL, labels = scales::comma) +
+    scale_y_log10(breaks = seq(1e4, 1e5, 1e4), minor_breaks = NULL, labels = scales::comma) +
     scale_color_brewer(type = "qual", palette = "Set1") +
     theme_bw()
 }
@@ -78,12 +65,14 @@ plot_latest <- function(data, color = NULL) {
 
 data <- get_data(file_db, years)
 
+weeks <- c("40-47 weeks", "48-49 weeks", "50-52 weeks")
+
 res <- data %>%
-  filter(sex == "male", age %in% 25:55, incwage > 0) %>%
-  calc_stats(by1 = "met2013", by2 = "met2013")
+  filter(sex == "male", age %in% 25:55, incwage > 0, wkswork2 %in% weeks, uhrswork >= 30) %>%
+  calc_stats(by = "met2013")
 
 res %>%
   filter(grepl("seattle|harrisburg|las vegas", met2013)) %>%
-  mutate(met2013 = substr(met2013, 1, 20)) %>%
+  mutate(met2013 = substr(met2013, 1, 15)) %>%
   mutate(met2013 = reorder(met2013, desc(q))) %>%
   plot_latest(color = "met2013")
