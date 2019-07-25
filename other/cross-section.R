1c81bdce14e2ce524ce9e5d7ba9dea35dd85b54f
Ray Wright <raywright@pa.gov>
2019-07-25 16:00:54 -0400

    Update script

--- other/cross-section.R
+++ other/cross-section.R
@@ -7,7 +7,8 @@ library(rwmisc)
 library(tidyr)
 
 file_db <- "~/data/acs/acsdb"
-years <- 2017
+years <- 2015:2017
+wage_fix <- 1.1
 
 # funs --------------------------------------------------------------------
 
@@ -54,7 +55,7 @@ plot_latest <- function(data, color = NULL) {
     geom_point(size = 1.5, alpha = 0.2) +
     geom_smooth(span = 0.5, se = FALSE, size = 1) +
     scale_x_continuous(limits = c(0, 1), breaks = seq(0, 1, 0.1), minor_breaks = NULL) +
-    scale_y_log10(breaks = seq(2e4, 2e5, 2e4), minor_breaks = NULL, labels = scales::comma) +
+    scale_y_log10(breaks = seq(2e4, 5e5, 2e4), minor_breaks = NULL, labels = scales::comma) +
     scale_color_brewer(type = "qual", palette = "Set1") +
     theme_bw()
 }
@@ -62,19 +63,30 @@ plot_latest <- function(data, color = NULL) {
 # run ---------------------------------------------------------------------
 
 data <- get_data(file_db, years)
+data$incwage <- data$incwage * wage_fix
 
 data2 <- filter(data, sex == "male", age %in% 25:35, incwage > 7500)
 
-all <- data2 %>%
-  calc_stats() %>%
+all <- calc_stats(data2) %>%
   mutate(met2013 = "all")
 
-data2 %>%
-  calc_stats(by = "met2013") %>%
-  filter(grepl("new york|harrisburg|las vegas", met2013)) %>%
+calc_stats(data2, by = "met2013") %>%
+  filter(grepl("harrisburg|las vegas", met2013)) %>%
   bind_rows(all) %>%
   mutate(met2013 = substr(met2013, 1, 15)) %>%
   mutate(met2013 = reorder(met2013, desc(q))) %>%
   plot_latest(color = "met2013") +
+  geom_hline(aes(yintercept = 60000), linetype = "dashed") +
+  geom_hline(aes(yintercept = 60000 * 1.3), linetype = "dashed")
+
+all <- calc_stats(data2) %>%
+  mutate(occ_cat_name = "all")
+
+calc_stats(data2, by = "occ_cat_name") %>%
+  filter(grepl("computer|business", occ_cat_name)) %>%
+  bind_rows(all) %>%
+  mutate(occ_cat_name = substr(occ_cat_name, 1, 15)) %>%
+  mutate(occ_cat_name = reorder(occ_cat_name, desc(q))) %>%
+  plot_latest(color = "occ_cat_name") +
   geom_hline(aes(yintercept = 60000), linetype = "dashed") +
   geom_hline(aes(yintercept = 60000 * 1.3), linetype = "dashed")
